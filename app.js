e0c539e93d3733f51ca22edebe72cecfd837f882
MURILO ANTUNES CAMPOS DA ROCHA <25203706@cfp127aluno.local>
2026-02-02 11:14:47 -0300

    adicionando a funcionalidade das cores vermelho, amarelo, verde e azul serem escritas em português

--- app.js
+++ app.js
@@ -5,8 +5,23 @@ const btTrocar = document.getElementById('trocar')
 
 function trocarCor(){
     console.log("testando botao")
-    const tInput = document.getElementById('input')
-    document.documentElement.style.setProperty('--cor-bg', tInput.value)
+    let tInput = document.getElementById('input').value.toLowerCase()
+
+    console.log(tInput)
+
+
+    if(tInput == "amarelo" || tInput.value == "Amarelo"){
+        tInput = "yellow" 
+        console.log(tInput.value)
+    }else if (tInput == "vermelho" || tInput == "Vermelho"){
+        tInput = "red"
+    }else if(tInput == "verde" || tInput == "Verde"){
+        tInput = "green"
+    }else if(tInput == "azul" || tInput == "Azul"){
+        tInput = "blue"
+    }
+    
+    document.documentElement.style.setProperty('--cor-bg', tInput)
 }
 
 btTrocar.addEventListener('click', trocarCor)
